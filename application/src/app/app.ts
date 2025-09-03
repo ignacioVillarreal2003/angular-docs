f0d0b5945b214dcd2d82983fa6348dca8bb711fe
ignacioVillarreal2003 <ignaciovillarreal20031231@gmail.com>
2025-09-03 12:49:38 -0300

    update

--- application/src/app/app.ts
+++ application/src/app/app.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RootFolder } from './core/models/root-folder';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('angular-docs');
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<RootFolder>('content/history.md').subscribe((data) => {
+      console.log(data);
+    }
+  }
 }
