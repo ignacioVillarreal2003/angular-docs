e9bd6c973f90a43e89457192712a2a8907032dbb
Ignacio Villarreal <ignaciovillarreal20031231@gmail.com>
2025-09-22 13:52:25 -0300

    update

--- application/src/app/features/folder/folder-item/folder-item.ts
+++ application/src/app/features/folder/folder-item/folder-item.ts
@@ -1,6 +1,7 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import {Folder} from '../../../core/models/folder';
 import {RouterLink} from '@angular/router';
+import ColorThief from 'colorthief';
 
 @Component({
   selector: 'app-folder-item',
@@ -12,4 +13,17 @@ import {RouterLink} from '@angular/router';
 })
 export class FolderItem {
   @Input() folder: Folder | undefined;
+
+    @ViewChild('folderImage2', { static: false }) folderImage!: ElementRef<HTMLDivElement>;
+  @ViewChild('coverImg2', { static: false }) coverImg!: ElementRef<HTMLImageElement>;
+
+  onImageLoad(img: HTMLImageElement) {
+    const colorThief = new ColorThief();
+    const color = colorThief.getColor(img);
+    const rgb = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+  
+    if (this.folderImage) {
+      this.folderImage.nativeElement.style.backgroundColor = rgb;
+    }
+  }
 }
--- application/src/app/features/folder/folder-page/folder-page.ts
+++ application/src/app/features/folder/folder-page/folder-page.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { Folder } from '../../../core/models/folder';
 import { ActivatedRoute } from '@angular/router';
 import { FolderService } from '../../../core/services/folder-service';
 import { FolderList } from '../folder-list/folder-list';
 import { DocumentList } from '../document-list/document-list';
+import ColorThief from 'colorthief';
 
 @Component({
   selector: 'app-folder-page',
@@ -12,11 +13,14 @@ import { DocumentList } from '../document-list/document-list';
   styleUrl: './folder-page.scss',
 })
 export class FolderPage {
-  folder: Folder | undefined;
+folder: Folder | undefined;
+
+  @ViewChild('folderImage', { static: false }) folderImage!: ElementRef<HTMLDivElement>;
+  @ViewChild('coverImg', { static: false }) coverImg!: ElementRef<HTMLImageElement>;
 
   constructor(private route: ActivatedRoute, private folderService: FolderService) {}
 
-  ngOnInit(): void {
+ ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       const path: string | null = params.get('path');
       if (path !== null) {
@@ -24,21 +28,22 @@ export class FolderPage {
           this.folder = folder;
           if (this.folder) {
             this.folder.folders =
-              this.folder.folders?.slice().sort((a, b) => {
-                const orderA = a.order ?? Infinity;
-                const orderB = b.order ?? Infinity;
-                return orderA - orderB;
-              }) ?? [];
-
+              this.folder.folders?.slice().sort((a, b) => (a.order ?? Infinity) - (b.order ?? Infinity)) ?? [];
             this.folder.documents =
-              this.folder.documents?.slice().sort((a, b) => {
-                const orderA = a.order ?? Infinity;
-                const orderB = b.order ?? Infinity;
-                return orderA - orderB;
-              }) ?? [];
+              this.folder.documents?.slice().sort((a, b) => (a.order ?? Infinity) - (b.order ?? Infinity)) ?? [];
           }
         });
       }
     });
   }
+
+  onImageLoad(img: HTMLImageElement) {
+  const colorThief = new ColorThief();
+  const color = colorThief.getColor(img);
+  const rgb = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+
+  if (this.folderImage) {
+    this.folderImage.nativeElement.style.backgroundColor = rgb;
+  }
+}
 }
